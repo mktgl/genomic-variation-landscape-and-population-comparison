9974bfd8481dfd1394934c9ab02333e112cb5fcb
sattwik-isr <sattwik.maji@students.iiserpune.ac.in>
2026-04-03 22:31:31 +0530

    test_aggregate.py

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -1,4 +1,8 @@
 # tests/test_aggregate.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 import pandas as pd
 from src.aggregate import compute_window_density, melt_population_af
