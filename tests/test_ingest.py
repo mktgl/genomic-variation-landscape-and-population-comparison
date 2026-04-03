5a39ca42187c708d6e24cf65645c87591efc3734
sattwik-isr <sattwik.maji@students.iiserpune.ac.in>
2026-04-03 22:32:19 +0530

    test_ingest.py

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -1,4 +1,8 @@
 # tests/test_ingest.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 import pandas as pd
 from pathlib import Path
